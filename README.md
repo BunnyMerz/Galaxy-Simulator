54c562f2852d136b08aa85febe56b7fc3c9dd9aa
BunnyMerz <tguerreirinhos.polo900@gmail.com>
2021-10-09 11:56:43 -0300

    Added comments, updated Readme, sligth change on main.py to fit another file from calling run()

--- README.md
+++ README.md
@@ -2,17 +2,23 @@
 
 #### Controls:
 
-Move around: W, A, D, S
-Zoom in/out: Up/S
+Move around: W, A, S, D
+Zoom in/out: Up/Down
 Reset Zoom: Left Control
 Show trace from planets: Q
 Hover over one of the planets on the top left and you will be fixed onto that specific one. Unhovering will undo it. You are allowed to move around even while hovered.
 
+
 #### Goals
+
 - Planet path predictor based off of a reference.
 - Galaxy Builder. An editor to easily create galaxys.
+- Deal with collision somehow.
+- Make the tracer better; Make it expand naturally as planets walk off-area.
+
 
 #### Known Problems:
+
 - Trace doesn't "proprely" show when zoomed out.
 - Zooming in and out may cause lag due to transformation of the Tracer.
 - Tracer may cause 'Out of memory'.
--- hud.py
+++ hud.py
@@ -8,22 +8,20 @@ class Selector():
         self.y = 0
         self.size = 0
         self.color = (50,100,120)
-        self.draw_f = lambda dummy : dummy ## Rewritten in unselect() and select()
 
     def unselect(self):
         self.size = 0
-        self.draw_f = lambda dummy : dummy
     
     def select(self,hover_block):
         bigger = 2
-        self.x = hover_block.x - bigger
-        self.y = hover_block.y - bigger
-        self.size = (hover_block.radius + bigger)*2
-
-        self.draw_f = lambda surface : py_draw.rect(surface,self.color,Rect(self.x,self.y,self.size,self.size),3)
+        self.x = hover_block.x - bigger ## Go left (will grow rigth afterwards)
+        self.y = hover_block.y - bigger ## Go up (Will grow down afterwards)
+        self.size = (hover_block.radius + bigger)*2 ## Grow down and rigth ## Square around circle. +2 so it doesn't overlap it.
     
     def draw(self,surface):
-        self.draw_f(surface)
+        if self.size == 0: ## self.size as 0 == hide
+            return
+        py_draw.rect(surface,self.color,Rect(self.x,self.y,self.size,self.size),3)
 
 class Hover_Button():
     def __init__(self, x, y, radius, color):
@@ -31,8 +29,10 @@ class Hover_Button():
         self.y = y
         self.color = color
         self.radius = radius
+        ##
         self.surface = Surface((radius*2,radius*2), SRCALPHA, 32)
         py_draw.circle(self.surface,self.color,(radius,radius),radius)
+        ##
     
     def is_hovering(self,mouse_pos):
         hb = [self.topleft(),self.bottomrigth()]
@@ -51,6 +51,10 @@ class Hover_Button():
         return (self.x+self.radius*2,self.y+self.radius*2)
         
 class Hud():
+    planet_size = 2
+    def planet_hud_scale(radius):
+        return sqrt(radius) * Hud.planet_size # Re-size the planets to use as mini version for the hud. Must change the return value here to affect everywhere (including logic).
+
     def __init__(self,galaxy,margin=13,starting_margin=10,y=5):
         self.hover_blocks = []
         self.selected = 0
@@ -58,10 +62,10 @@ class Hud():
 
         self.galaxy = galaxy
         x = starting_margin
-        y += max([sqrt(x.radius)*3 for x in galaxy.planets])
+        y += max([ Hud.planet_hud_scale(x.radius) for x in galaxy.planets]) # Get biggest size of all planets (re-scaled version) to determine how to center other planets
         for planet in galaxy.planets:
-            r = sqrt(planet.radius) * 3
-            h = Hover_Button(x=x, y=y-r, radius=r, color=planet.color)
+            r = Hud.planet_hud_scale(planet.radius) ## re-scaled radius
+            h = Hover_Button(x=x, y=y-r, radius=r, color=planet.color) # text-align: center
             self.hover_blocks.append(h)
             x += r*2 + margin
 
@@ -71,7 +75,7 @@ class Hud():
     def hitbox(self):
         left = (self.hover_blocks[0].topleft())
         rigth = (self.hover_blocks[-1].bottomrigth())
-        return [left,rigth]
+        return [left,rigth] ## Top most of the whole box (First planet's topleft) and bottom most.
     
     def draw(self,surface):
         for hb in self.hover_blocks:
@@ -80,25 +84,16 @@ class Hud():
     
     def update(self,mouse_pos=[-1,-1]):
         hb = self.hitbox()
+        ## Check big hitbox (1 collision detection instead of len(hover_blocks))
         if ((mouse_pos[0] >= hb[0][0] and mouse_pos[0] <= hb[1][0]) and (
              mouse_pos[1] >= hb[0][1] and mouse_pos[1] <= hb[1][1])):
             x = 0
-            for hover in self.hover_blocks:
+            for hover in self.hover_blocks: ## Individual Hovers. Could be better and use more divisions, but unecessery as of now.
                 if hover.is_hovering(mouse_pos):
-                    self.selector.select(hover)
-                    self.galaxy.set_reference(self.galaxy.planets[x])
-                    return
+                    self.selector.select(hover) ## Show Select box over the selected one
+                    self.galaxy.set_reference(self.galaxy.planets[x]) ## Lock the camera onto the planet
+                    return ## Unecessery check after this if, as it will all be false.
                 x+=1
-        self.selector.unselect()
-        self.galaxy.unset_reference()
- 
-    # def draw(self,surface,mouse_pos=[-1,-1]):
-    #     for x in range(len(self.hover_blocks)):
-    #         if self.hover_blocks[x].is_hovering(mouse_pos):
-    #             self.series[x].draw(surface)
-    #             self.draw_hud(surface)
-    #             return
-    #     for serie in self.series:
-    #         serie.draw(surface)
-    #     self.draw_hud(surface)
-        
+        ## Only will reach end if not hovering.
+        self.selector.unselect() ## Hide selector
+        self.galaxy.unset_reference() ## Unlock camera. Migth cause trouble if something else aside from hover sets the reference. Later one, it will be used clicking instead of hovering, so this should not happen anymore.
